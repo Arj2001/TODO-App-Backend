d3c08ff977678d37e47a92d68207bc779708e16c
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-25 14:37:13 +0530

    1st

---
+++ src/test/java/com/nestgroup/TODO/App/Backend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestgroup.TODO.App.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
